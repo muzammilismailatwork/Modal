b652291c5ba4e4f2f1146104be82f6b79f0cd733
Muzammil Soomro <muzammilsoomro4321@gmail.com>
2025-01-28 16:25:41 +0500

    Modal

---
+++ app.js
@@ -0,0 +1,9 @@
+
+function hide(){
+    let modal = document.querySelector(".modal");
+    modal.classList.remove("show")
+}
+function show(){
+    let modal = document.querySelector(".modal");
+    modal.classList.add("show")
+}
